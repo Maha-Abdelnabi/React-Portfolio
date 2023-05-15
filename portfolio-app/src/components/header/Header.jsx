090839a8a3e915328795a4cde2ff02705f293116
Maha Abdelnabi <mahaomar@gmail.com>
2023-05-15 03:27:27 -0400

    add content to header component and create more components related to the header

--- portfolio-app/src/components/header/Header.jsx
+++ portfolio-app/src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div className='header'>Header</div>
+    <Header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Maha Abdelnabi</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src="#" alt="me" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </Header>
   )
 }
 
