2e6c5e9982905033edf6e1839df7eec60ed502d0
Maha Abdelnabi <mahaomar@gmail.com>
2023-05-15 23:34:28 -0400

    add content and dtyling to nav and about components

--- portfolio-app/src/components/header/Header.jsx
+++ portfolio-app/src/components/header/Header.jsx
@@ -2,22 +2,25 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA';
 import HeaderSocials from './HeaderSocials';
+import Image  from "../../assets/11.png";
+
 
 const Header = () => {
   return (
-      <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Maha Abdelnabi</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
-        <CTA />
-        <HeaderSocials />
-        <div className="me">
-          <img src="#" alt="me" />
-        </div>
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+    <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>Maha Abdelnabi</h1>
+      <h5 className="text-light">Fullstack Developer</h5>
+      <CTA />
+      <HeaderSocials />
+      <div className="me">
+        <img src={Image} alt="me" />
       </div>
-    
-  )
+      <a href="#contact" className="scroll__down">
+        Scroll Down
+      </a>
+    </div>
+  );
 }
 
 export default Header;
--- portfolio-app/src/components/nav/Nav.jsx
+++ portfolio-app/src/components/nav/Nav.jsx
@@ -1,10 +1,54 @@
 import React from 'react'
 import './nav.css'
+import {RiHome8Line} from 'react-icons/ri'
+import { FiUser } from "react-icons/fi";
+import { MdMiscellaneousServices } from "react-icons/md";
+import { FaRegPaperPlane } from "react-icons/fa";
+import { ImNewspaper } from "react-icons/im";
+import { useState } from 'react';
+
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <RiHome8Line />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FiUser />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <MdMiscellaneousServices />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <FaRegPaperPlane />
+      </a>
+      <a
+        href="#resume"
+        onClick={() => setActiveNav("#resume")}
+        className={activeNav === "#resume" ? "active" : ""}
+      >
+        <ImNewspaper />
+      </a>
+    </nav>
+  );
 }
 
 export default Nav;
